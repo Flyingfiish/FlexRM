f514e39889d39af56e2aa78d5a6af7b37d2519ab
e.gabdeev <eldarg76@gmail.com>
2022-09-14 20:18:17 +0500

    FXRM-9 refactor

--- FlexRM.Core/FlexRM.Core.Tests/GenerationTests/CreateEntitySource.cs
+++ FlexRM.Core/FlexRM.Core.Tests/GenerationTests/CreateEntitySource.cs
@@ -1,9 +1,9 @@
 using FlexRM.Core.Application.Generation;
 using FlexRM.Core.Domain.Entities.Configuration;
 
-namespace FlexRM.Core.Tests
+namespace FlexRM.Core.Tests.GenerationTests
 {
-    public class Tests
+    public class CreateEntitySource
     {
         [SetUp]
         public void Setup()
@@ -11,12 +11,15 @@ namespace FlexRM.Core.Tests
         }
 
         [Test]
-        public void Test1()
+        public void CreateSimpleEntity()
         {
             var entity = new Entity() { Id = Guid.NewGuid(), Name = "Test" };
             entity.Columns.Add(new Column() { Name = "TestColumn", Type = Domain.Enums.ColumnTypes.String });
+
             var generator = new EntitySourceGenerator(entity);
+            var expected = "using System;\r\nusing System.Collections.Generic;\r\nusing System.Linq;\r\nusing System.Text;\r\nusing System.Threading.Tasks;\r\n\r\nnamespace FlexRM.Generated.Domain.Entities\r\n{\r\n    public class Test\r\n    {\r\n        public string TestColumn { get; set; }\r\n    }\r\n}";
             var result = generator.Generate();
+            Assert.AreEqual(expected, result);
         }
     }
 }
